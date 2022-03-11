ec034eea3b3750816c6618855523c95097b21c23
Diego Felix <dt196397@gmail.com>
2022-03-10 20:14:41 -0600

    Contructed app

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const coolRouter = require('./routes/cool');
+const newEntry = require('./routes/newEntry');
+
+const User = require('./User');
 
 const app = express();
 
+// Set up mongoose
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@cluster0.qyah1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => {
+  console.log('connected');
+});
+
+async function run() {
+  const user = new User({ name: 'Richard', age: 25 });
+  await user.save();
+  console.log(user);
+}
+// run();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +36,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/newEntry', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users/cool', coolRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -37,5 +55,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000);
 
 module.exports = app;
