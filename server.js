bba77f68b6318ee55b8c0dee901e24aa89268f1f
Diego Felix <dt196397@gmail.com>
2022-03-22 14:35:27 -0600

    update API keys variables

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path  = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const aws = require('aws-sdk');
 
 const app = express();
 
@@ -11,12 +12,17 @@ const apiRouter = require('./routes/api')
 
 const User = require('./User');
 
-
+let s3 = new aws.S3({
+  MONGODB_USERNAME: process.env.S3_KEY,
+  MONGODB_PASS: process.env.S3_SECRET
+});
 // Set up mongoose
-const mongodb = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.qyah1.mongodb.net/personalLibrary?retryWrites=true&w=majority';
+const mongodb = `mongodb+srv://${s3.MONGODB_USERNAME}:${s3.MONGODB_PASS}@cluster0.qyah1.mongodb.net/personalLibrary?retryWrites=true&w=majority`;
 mongoose.connect(mongodb, () => {
   console.log('connected');
 });
+
+
 
 //view engine
 app.set("view engine", "ejs");
